cb67902df029725222e01850801802deeb501200
Amber Jankowski <amberbjankowski@gmail.com>
2024-01-18 08:47:44 -0500

    files set up

---
+++ helper.js
@@ -0,0 +1,11 @@
+function wordsArr(){
+    var fs = require('fs')
+    var text = fs.readFileSync("./sowpods.txt").toString("utf-8")
+    var splitWords = text.split("\n")
+    var words = splitWords.map(function(word){
+        return word.trim()
+    })
+    return words
+}
+
+module.exports = wordsArr
---
+++ wordPlaySolutions/solution1.js
@@ -0,0 +1,3 @@
+const wordsArr = require('../helper.js')
+const words = wordsArr()
+console.log(words)
