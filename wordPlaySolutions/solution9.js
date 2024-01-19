9099c54708a514d4846a4cdd265a7b2b2bb9c755
Amber Jankowski <amberbjankowski@gmail.com>
2024-01-19 11:29:07 -0500

    solution 9 complete, words that have all 5 vowels in any order

---
+++ wordPlaySolutions/solution9.js
@@ -0,0 +1,13 @@
+const wordsArr = require('../helper')
+const words = wordsArr()
+
+// words that have all 5 vowels, in any order
+
+
+for(let i = 0; i < words.length; i++){
+    let word = words[i].split('')
+
+  if(word.includes('A') && word.includes('E') && word.includes('I') && word.includes('O') && word.includes('U')){
+    console.log(words[i])
+  }
+}
