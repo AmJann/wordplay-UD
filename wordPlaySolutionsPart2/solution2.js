19839b517d29e278ffecc33fecf4722afa4c6308
Amber Jankowski <amberbjankowski@gmail.com>
2024-01-22 18:23:10 -0500

    simplified solution for part 2 solution 2

--- wordPlaySolutionsPart2/solution2.js
+++ wordPlaySolutionsPart2/solution2.js
@@ -3,14 +3,10 @@ const words = wordsArr();
 
 // Create and print an array containing all of the words that end in "GHTLY"
 
-let answerArr = [];
-
 for (let i = 0; i < words.length; i++) {
   let word = words[i];
 
   if (word.endsWith("GHTLY")) {
-    answerArr.push(word);
+    console.log(word);
   }
 }
-
-console.log(answerArr);
