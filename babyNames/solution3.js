17c41f2e2e124f61829577efbffd38242ec710bb
Amber Jankowski <amberbjankowski@gmail.com>
2024-02-23 17:40:04 -0500

    solution implementing map object added

--- babyNames/solution3.js
+++ babyNames/solution3.js
@@ -15,18 +15,74 @@ const words = wordsArr();
 //compare it to each word in scrabble words
 //console.log each reverse name that matches a word
 
-function arrayAnswer(arr1, arr2) {
-  console.log(performance.now());
-  for (let i = 0; i < arr1.length; i++) {
-    let splitName = arr1[i].split("");
-    let reverse = splitName.reverse().join("").toUpperCase();
-    for (let j = 0; j < arr2.length; j++) {
-      if (reverse === arr2[j]) {
-        console.log(arr1[i]);
+function reverse(word) {
+  return word.split("").reverse().join("").toUpperCase();
+}
+//array solution
+function arrayAnswer(babyNames, words) {
+  const t0 = performance.now();
+  for (let i = 0; i < babyNames.length; i++) {
+    let reversed = reverse(babyNames[i]);
+    for (let j = i + 1; j < words.length; j++) {
+      if (reversed === words[j]) {
+        console.log(babyNames[i]);
       }
     }
   }
-  console.log(performance.now());
+  const t1 = performance.now();
+  console.log(`array solution took ${t1 - t0} milliseconds.`);
 }
 
 arrayAnswer(babyNames, words);
+
+//set solution
+
+function setAnswer(babyNames, words) {
+  const t0 = performance.now();
+  let set = new Set(words);
+
+  for (const name of babyNames) {
+    let reversed = reverse(name);
+
+    if (set.has(reversed)) {
+      console.log(name);
+    }
+  }
+  const t1 = performance.now();
+  console.log(`set solution took ${t1 - t0} milliseconds.`);
+}
+
+setAnswer(babyNames, words);
+
+//map answer
+
+//loop through and insert words into map
+//separate loop through to compare reversed baby name to scrabble word
+//if reversed name is equal to scrabble word, console.log baby name
+// add in performance.now() to before and after solution
+//console.log(t0-t1)
+
+function createMap(arr) {
+  let map = {};
+  for (const item of arr) {
+    if (item) {
+      map[item] = true;
+    }
+  }
+  return map;
+}
+function mapAnswer(babyNames, words) {
+  t0 = performance.now();
+  let map = createMap(words);
+  for (const name of babyNames) {
+    let reversed = reverse(name);
+    if (map[reversed] !== undefined) {
+      console.log(name);
+    }
+  }
+  t1 = performance.now();
+  console.log(`It took ${t1 - t0} milliseconds to run map solution`);
+  return 5;
+}
+
+mapAnswer(babyNames, words);
