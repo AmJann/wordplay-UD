4b674b236b19fa6eb965a8f6d99f2b230de4d202
Amber Jankowski <amberbjankowski@gmail.com>
2024-04-18 18:19:23 -0400

    request to pokemon api returns pokemon input with timeout set

--- interview/pokemonApi.js
+++ interview/pokemonApi.js
@@ -13,23 +13,54 @@ held_items[index].item.name - would return items
 
 const url = "https://pokeapi.co/api/v2/";
 let pokemonName = "clefairy";
-const endpoint = `pokemon/${pokemonName}`;
+const endpoint = `pokemon/`;
 
-const getPokemonItems = async () => {
-  let res = await axios.get(url + endpoint);
-
-  res.data.held_items.forEach((item) => {
-    console.log(item["item"]["name"]);
-  });
-  console.log(res.data);
-};
+// const getPokemonItems = async () => {
+//   let res = await axios.get(url + endpoint);
+//   let items = [];
+//   res.data.held_items.map((item) => {
+//     items.push(item["item"]["name"]);
+//   });
+//   console.log(res.data);
+// };
 
 // getPokemonItems();
 
-const names = ["jake", "paul", "stacy", "john", "emma"];
+/* 
 
-{
-  names.map((name, key) => {
-    console.log(name);
+-Return an array using the help of .map()
+-Function should accept an argument for pokemonName
+-Use axios.send() instead of axios.get(), and pass into axios.send() a config object
+-Let’s set a timeout: 2000
+
+*/
+
+const sendPokemonAPI = async (pokemonName) => {
+  const res = await axios.request({
+    method: "get",
+    url: url + endpoint + pokemonName,
+    headers: { "Content-Type": "application/json" },
+    timeout: 2000,
   });
+  let items = res.data.held_items.map((item) => {
+    return item["item"]["name"];
+  });
+  console.log(items);
+  return items;
+};
+
+async function doThings() {
+  const myItems = await sendPokemonAPI("clefairy");
+  console.log(myItems);
 }
+
+doThings();
+// sendPokemonAPI("clefairy");
+
+// const names = ["jake", "paul", "stacy", "john", "emma"];
+
+// {
+//   names.map((name, key) => {
+//     console.log(name);
+//   });
+// }
